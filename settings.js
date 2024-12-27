67ef3ee1dec411c5e9a289b2d204621d39b6284c
LearnOnix <surya01785@gmail.com>
2024-12-28 05:04:10 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'My Sticker Pack',
+  author: 'My Bot',
+  botName: "SsSps Bot",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '917571952689', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
