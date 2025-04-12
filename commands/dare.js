9431191b92ef4ef407b7ccac01d54d6d806b1694
LearnOnix <surya01785@gmail.com>
2025-04-13 01:04:31 +0530

    Update dare.js

--- commands/dare.js
+++ commands/dare.js
@@ -1,7 +1,7 @@
 const dares = [
     "Sing a song for the group!",
     "Do 10 push-ups.",
-    "Talk in a funny accent for the next 5 minutes.",
+    "Talk in a funny accent for the next 5 minutes!",
     "Send a selfie doing a funny face.",
     "Let someone text anything they want from your phone."
 ];
